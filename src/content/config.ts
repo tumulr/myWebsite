af8b3fc10d192a67062118aaa5ecae0892e9b9bf
Tumul Richesh <tumul.richesh@dat.com>
2025-08-16 07:19:23 +0530

    chore: indent 4 space to config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,12 @@
 import { defineCollection, z } from 'astro:content';
 
 const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-  }),
+    schema: z.object({
+        title: z.string(),
+        date: z.string(),
+    }),
 });
 
 export const collections = {
-  'blogs': blogCollection,
+    'blogs': blogCollection,
 };
